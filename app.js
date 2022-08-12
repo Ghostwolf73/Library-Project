fb4bf76a5211eb127853ff95e721d55667e64320
Ghostwolf73 <solomzigumbo@gmail.com>
2022-08-12 13:37:57 +0200

    added read button

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
-let myLibrary = [];
 
-class Book {
-    constructor(title, author, pages, status){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.status = status;
+let books = [];
+
+function popForm() {
+    $("#popup").removeClass("hide");
+}
+
+function hideForm() {
+    $("#popup").addClass("hide");
+    $("#main-page").removeClass("hide");
+}
+
+function toggle() {
+    $("#main-page").addClass("hide");
+
+}
+
+function addBookToTable(book) {
+    let table = document.getElementById("di-books");
+
+    let tr = document.createElement("tr");
+    Object.values(book).forEach((value) => {
+        let td = document.createElement("td");
+        td.innerHTML = value;
+        tr.appendChild(td);
+    });
+    table.appendChild(tr);
+
+}
+
+// then use it in your addBook function 
+const addBook = (event) => {
+    event.preventDefault();
+    let book = {
+        title: $("#title").val(),
+        author: $("#author").val(),
+        pages: $("#pages").val(),
+        status: $("#status").val(),
+    };
+    const cb = document.querySelector("#status");
+
+    if (cb.checked === true) {
+        book.status = "read"
+    } else {
+        book.status = "not read"
     }
-
-
-}
-
-var bookOne = new Book('Naruto', 'Kishimoto', 98, true);
-var bookTWo = new Book('One Piece', 'Oda', 300, false);
-
-function addBookToLibrary() {
-    // const newBook = new Book($title.title, $author.author, $pages.pages, $status.status);
-    // myLibrary.push(newBook);
-    myLibrary.push(bookOne, bookTWo);
-    console.log(myLibrary)
-}
-
-addBookToLibrary();
-
-
-
+    books.push(book);
+    document.forms[0].reset();
+    // Update DOM
+    addBookToTable(book)
+    localStorage.setItem("myMangaList", JSON.stringify(books));
+};
